76c86c51c7e098e956336f0cc7660fd4760a7029
haganenoubik <haganenoubik@gmail.com>
2024-05-12 16:33:35 +0900

    :bug: Fix: baseオプションをコメントアウトし、URLパスの起点を修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://haganenoubik.github.io",
-  base: "/astro_tutorial",
+  // base: "/astro_tutorial",
 });
